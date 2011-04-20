8c51a7aaf04d4404510af3171ff3e6743550c400
Dave Dulson <dave@dulson.com>
2011-04-20 18:22:24 +0100

    Add better command line handling

--- CodeJam/Program.cs
+++ CodeJam/Program.cs
@@ -12,23 +12,33 @@ namespace CodeJam
         {
             TextWriter output;
 
-            if (args.Length > 2 && args[2].Equals( "debug" ))
+            if (args.Length > 2)
             {
-                output = System.Console.Out;
+
+                if (args.Length > 2 && args[2].Equals( "debug" ))
+                {
+                    output = System.Console.Out;
+                }
+                else
+                {
+                    output = new StreamWriter( string.Concat( desktop, "/output.txt" ) );
+                }
+
+                switch (args[0])
+                {
+                    case "alien":
+                        runAlienLanguage( args[1], output );
+                        break;
+                    default:
+                        System.Console.WriteLine( "Unknown problem: {0}", args[0] );
+                        break;
+                }
             }
             else
             {
-                output = new StreamWriter( string.Concat( desktop, "/output.txt" ) );
-            }
-
-            switch (args[0])
-            {
-                case "alien":
-                    runAlienLanguage( args[1], output );
-                    break;
-                default:
-                    System.Console.WriteLine( "Unknown problem: {0}", args[0] );
-                    break;
+                System.Console.WriteLine( "Requires two command line arguments: The problem, and the input file" );
+                System.Console.WriteLine( "Currently accepted problems are:" );
+                System.Console.WriteLine( "alien" );
             }
         }
 
